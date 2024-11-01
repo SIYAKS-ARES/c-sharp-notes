418db62fc8ab16dbdd029e795bb6716fecdeeb2f
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-11-01 09:57:31 +0300

    BackUp

---
+++ side-projects/Flappy Bird Windows Form Yedek/Flappy Bird Windows Form/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Flappy_Bird_Windows_Form
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ustBoru = new System.Windows.Forms.PictureBox();
+            this.altBoru = new System.Windows.Forms.PictureBox();
+            this.kus = new System.Windows.Forms.PictureBox();
+            this.zemin = new System.Windows.Forms.PictureBox();
+            this.puanMetni = new System.Windows.Forms.Label();
+            this.oyunZamani = new System.Windows.Forms.Timer(this.components);
+
+            ((System.ComponentModel.ISupportInitialize)(this.ustBoru)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altBoru)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).BeginInit();
+            this.SuspendLayout();
+            
+            // ustBoru
+            this.ustBoru.Image = global::Flappy_Bird_Windows_Form.Properties.Resources.pipedown;
+            this.ustBoru.Location = new System.Drawing.Point(495, -59);
+            this.ustBoru.Name = "ustBoru";
+            this.ustBoru.Size = new System.Drawing.Size(100, 266);
+            this.ustBoru.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ustBoru.TabIndex = 0;
+            this.ustBoru.TabStop = false;
+
+            // altBoru
+            this.altBoru.Image = global::Flappy_Bird_Windows_Form.Properties.Resources.pipe;
+            this.altBoru.Location = new System.Drawing.Point(362, 418);
+            this.altBoru.Name = "altBoru";
+            this.altBoru.Size = new System.Drawing.Size(109, 286);
+            this.altBoru.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.altBoru.TabIndex = 1;
+            this.altBoru.TabStop = false;
+
+            // kus
+            this.kus.Image = global::Flappy_Bird_Windows_Form.Properties.Resources.bird;
+            this.kus.Location = new System.Drawing.Point(69, 228);
+            this.kus.Name = "kus";
+            this.kus.Size = new System.Drawing.Size(82, 69);
+            this.kus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kus.TabIndex = 2;
+            this.kus.TabStop = false;
+
+            // zemin
+            this.zemin.Image = global::Flappy_Bird_Windows_Form.Properties.Resources.ground;
+            this.zemin.Location = new System.Drawing.Point(-16, 633);
+            this.zemin.Name = "zemin";
+            this.zemin.Size = new System.Drawing.Size(655, 126);
+            this.zemin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.zemin.TabIndex = 3;
+            this.zemin.TabStop = false;
+
+            // puanMetni
+            this.puanMetni.AutoSize = true;
+            this.puanMetni.BackColor = System.Drawing.Color.Moccasin;
+            this.puanMetni.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puanMetni.Location = new System.Drawing.Point(181, 661);
+            this.puanMetni.Name = "puanMetni";
+            this.puanMetni.Size = new System.Drawing.Size(122, 37);
+            this.puanMetni.TabIndex = 4;
+            this.puanMetni.Text = "Puan: 0";
+
+            // oyunZamani
+            this.oyunZamani.Enabled = true;
+            this.oyunZamani.Interval = 20;
+            this.oyunZamani.Tick += new System.EventHandler(this.oyunZamaniEtkinlik);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(584, 761);
+            this.Controls.Add(this.puanMetni);
+            this.Controls.Add(this.kus);
+            this.Controls.Add(this.altBoru);
+            this.Controls.Add(this.ustBoru);
+            this.Controls.Add(this.zemin);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tusBasildi);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tusBirakildi);
+
+            ((System.ComponentModel.ISupportInitialize)(this.ustBoru)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altBoru)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ustBoru;
+        private System.Windows.Forms.PictureBox altBoru;
+        private System.Windows.Forms.PictureBox kus;
+        private System.Windows.Forms.PictureBox zemin;
+        private System.Windows.Forms.Label puanMetni;
+        private System.Windows.Forms.Timer oyunZamani;
+    }
+}
---
+++ side-projects/Flappy Bird Windows Form Yedek/Flappy Bird Windows Form/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media; // Ses eklemek için gerekli kütüphane
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_Bird_Windows_Form
+{
+    public partial class Form1 : Form
+    {
+        // Değişkenler burada başlar
+        int boruHizi = 8; // Varsayılan boru hızı
+        int yercekimi = 15; // Varsayılan yerçekimi
+        int puan = 0; // Varsayılan puan
+        bool oyunBasladi = false; // Oyunun başlangıç durumu kontrolü
+
+        SoundPlayer kanatSesi = new SoundPlayer("flap.wav"); // Kanat sesi
+        SoundPlayer carpismaSesi = new SoundPlayer("hit.wav"); // Çarpma sesi
+        SoundPlayer puanSesi = new SoundPlayer("score.wav"); // Puan alma sesi
+        SoundPlayer oyunBittiSesi = new SoundPlayer("gameover.wav"); // Oyun sonu sesi
+
+        public Form1()
+        {
+            InitializeComponent();
+            oyunZamani.Stop(); // Oyun başlangıçta durdurulmuş olacak
+            BaslangicEkrani(); // Başlangıç ekranını göster
+        }
+
+        private void tusBasildi(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && oyunBasladi)
+            {
+                yercekimi = -15; // Kuş yukarı çıkmaya başlar
+                kanatSesi.Play(); // Kanat sesini çal
+            }
+
+            if (e.KeyCode == Keys.Space && !oyunBasladi)
+            {
+                OyunuBaslat(); // Oyun henüz başlamamışsa başlat
+            }
+        }
+
+        private void tusBirakildi(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                yercekimi = 15; // Kuş tekrar aşağı düşer
+            }
+        }
+
+        private void BaslangicEkrani()
+        {
+            // Başlangıç ekranı ayarları
+            puanMetni.Text = "Başlamak için BOŞLUK tuşuna basın";
+            kus.Visible = false;
+            altBoru.Visible = false;
+            ustBoru.Visible = false;
+            zemin.Visible = false;
+        }
+
+        private void OyunuBaslat()
+        {
+            // Oyunu başlat
+            oyunBasladi = true;
+            puan = 0;
+            yercekimi = 15;
+            boruHizi = 8;
+            kus.Top = 228;
+            altBoru.Left = 800;
+            ustBoru.Left = 950;
+            oyunZamani.Start();
+            kus.Visible = true;
+            altBoru.Visible = true;
+            ustBoru.Visible = true;
+            zemin.Visible = true;
+            puanMetni.Text = "Puan: 0";
+        }
+
+        private void OyunuBitir()
+        {
+            oyunZamani.Stop(); // Zamanlayıcı durur
+            oyunBittiSesi.Play(); // Oyun sonu sesi çal
+            puanMetni.Text += " Oyun bitti!!! Yeniden başlatmak için R'ye bas"; // Oyun bitti mesajı
+        }
+
+        private void oyunZamaniEtkinlik(object sender, EventArgs e)
+        {
+            kus.Top += yercekimi; // Kuşun hareketi
+            altBoru.Left -= boruHizi; // Boruların hareketi
+            ustBoru.Left -= boruHizi;
+
+            // Puanı göster
+            puanMetni.Text = "Puan: " + puan;
+
+            // Borular ekrandan çıkarsa
+            if (altBoru.Left < -150)
+            {
+                altBoru.Left = 800;
+                puan++;
+                puanSesi.Play(); // Puan sesi çal
+            }
+            if (ustBoru.Left < -180)
+            {
+                ustBoru.Left = 950;
+                puan++;
+                puanSesi.Play();
+            }
+
+            // Çarpışma kontrolü
+            if (kus.Bounds.IntersectsWith(altBoru.Bounds) ||
+                kus.Bounds.IntersectsWith(ustBoru.Bounds) ||
+                kus.Bounds.IntersectsWith(zemin.Bounds) ||
+                kus.Top < -25)
+            {
+                carpismaSesi.Play(); // Çarpma sesi çal
+                OyunuBitir(); // Oyunu bitir
+            }
+
+            // Puan 5'i geçerse boru hızı artar
+            if (puan > 5)
+            {
+                boruHizi = 15;
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'r' && !oyunBasladi)
+            {
+                OyunuBaslat(); // 'R' tuşuna basılırsa oyun yeniden başlar
+            }
+        }
+    }
+}
