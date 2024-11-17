a219a78e55175b72d8a2c3e2cabe7a57e01bd6bc
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-11-17 10:23:09 +0300

    TicTacToe Game wiith C#

---
+++ side-projects/TicTacToeGameWithCSharp/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace TicTacToeGameWithCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.OyunZamanı = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(40, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Oyuncu Kazanır:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(373, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bilgisayar Kazanır:";
+            //this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(44, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 152);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.OyuncuTiklamaButonu);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(211, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 152);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.OyuncuTiklamaButonu);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(378, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 152);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.OyuncuTiklamaButonu);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Silver;
+            this.button4.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(44, 245);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 152);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.OyuncuTiklamaButonu);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Silver;
+            this.button5.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(211, 245);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 152);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.OyuncuTiklamaButonu);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Silver;
+            this.button6.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(378, 245);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(160, 152);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.OyuncuTiklamaButonu);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Silver;
+            this.button7.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(44, 398);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(160, 152);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.OyuncuTiklamaButonu);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Silver;
+            this.button8.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(211, 398);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(160, 152);
+            this.button8.TabIndex = 9;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.OyuncuTiklamaButonu);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Silver;
+            this.button9.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(378, 398);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(160, 152);
+            this.button9.TabIndex = 10;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.OyuncuTiklamaButonu);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button10.Location = new System.Drawing.Point(230, 556);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(117, 53);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Tekrar Oyna";
+            this.button10.UseVisualStyleBackColor = false;
+            //this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // OyunZamanı
+            // 
+            this.OyunZamanı.Interval = 1000;
+            this.OyunZamanı.Tick += new System.EventHandler(this.BilgisayarOynar); // Corrected the event hookup
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(584, 661);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Timer OyunZamanı;
+    }
+}
---
+++ side-projects/TicTacToeGameWithCSharp/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TicTacToeGameWithCSharp
+{
+    public partial class Form1 : Form
+    {
+        // Oyuncu sınıfı aşağıda belirtilmiştir. X ve O değerleri vardır.
+        // Bu şekilde oyuncu ve yapay zeka sembollerini kontrol edebiliriz.
+        public enum Oyuncu
+        {
+            X, O
+        }
+
+        Oyuncu suankiOyuncu; // Oyuncu sınıfını çağırıyoruz
+        List<Button> buttons; // Butonları liste (array) olarak oluşturuyoruz
+        Random random = new Random(); // Rastgele sayı üreteci sınıfını kullanıyoruz
+        int oyuncuKazanma = 0; // Oyuncu kazanma sayısı sıfır olarak ayarlanır
+        int bilgisayarKazanma = 0; // Bilgisayar kazanma sayısı sıfır olarak ayarlanır
+        Timer yapayZekaHamleleri; // Yapay zekâ hamleleri için bir zamanlayıcı (timer)
+
+        public Form1()
+        {
+            InitializeComponent();
+            OyunuTekrarBaslat(); // Oyunu başlatan fonksiyonu çağırır
+            YapayZekaZamanlayicisiniAyarla(); // Zamanlayıcıyı ayarla
+        }
+
+        private void YapayZekaZamanlayicisiniAyarla()
+        {
+            yapayZekaHamleleri = new Timer
+            {
+                Interval = 1000 // 1 saniyelik zamanlayıcı
+            };
+            yapayZekaHamleleri.Tick += BilgisayarOynar; // Tick olayında BilgisayarOynar metodunu çağırır
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void OyuncuTiklamaButonu(object sender, EventArgs e)
+        {
+            var button = (Button)sender; // Hangi butona tıklandığını bulur
+            suankiOyuncu = Oyuncu.X; // Oyuncuyu X olarak ayarlar
+            button.Text = suankiOyuncu.ToString(); // Butonun metnini X olarak değiştirir
+            button.Enabled = false; // Butonu devre dışı bırakır
+            button.BackColor = System.Drawing.Color.Cyan; // Oyuncunun rengini Mavi olarak değiştirir
+            buttons.Remove(button); // Butonu listeden kaldırarak yapay zekânın seçememesini sağlar
+            OyunuKontrolEt(); // Oyuncunun kazanıp kazanmadığını kontrol eder
+            yapayZekaHamleleri.Start(); // Yapay zekâ zamanlayıcısını başlatır
+        }
+
+        private void BilgisayarOynar(object sender, EventArgs e)
+        {
+            // Yapay zekâ, buton listesinden rastgele bir seçim yapacak.
+            if (buttons.Count > 0)
+            {
+                int index = random.Next(buttons.Count); // Mevcut buton sayısı içinde rastgele bir sayı üretir
+                buttons[index].Enabled = false; // Üretilen sayıya göre butonu seçer
+                suankiOyuncu = Oyuncu.O; // Yapay zekâyı O olarak ayarlar
+                buttons[index].Text = suankiOyuncu.ToString(); // Butona O yazar
+                buttons[index].BackColor = System.Drawing.Color.DarkSalmon; // Butonun rengini Koyu Somon yapar
+                buttons.RemoveAt(index); // O butonu listeden çıkarır
+                OyunuKontrolEt(); // Yapay zekânın kazanıp kazanmadığını kontrol eder
+                yapayZekaHamleleri.Stop(); // Yapay zekâ zamanlayıcısını durdurur
+            }
+        }
+
+        private void OyunuBastanBaslat(object sender, EventArgs e)
+        {
+            // Bu fonksiyon sıfırla butonuna bağlıdır.
+            OyunuTekrarBaslat();
+        }
+
+        private void ButonlariYukle()
+        {
+            // Bu özel fonksiyon formdaki butonları butonlar listesine yükler
+            buttons = new List<Button> { button1, button2, button3, button4, button5, button6, button7, button8, button9 };
+        }
+
+        private void OyunuTekrarBaslat()
+        {
+            // 'oyna' etiketi olan tüm butonları kontrol eder
+            foreach (Control X in this.Controls)
+            {
+                if (X is Button && X.Tag == "oyna")
+                {
+                    ((Button)X).Enabled = true; // Tüm butonları etkinleştirir
+                    //((Button)X).Text = "?"; // Butonun metnini soru işaretine çevirir
+                    ((Button)X).BackColor = default; // Buton rengini varsayılana çevirir
+                }
+            }
+            ButonlariYukle(); // Butonları listeye yeniden ekler
+        }
+
+        private void OyunuKontrolEt()
+        {
+            // Bu fonksiyon oyuncunun ya da yapay zekânın kazanıp kazanmadığını kontrol eder
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X"
+               || button4.Text == "X" && button5.Text == "X" && button6.Text == "X"
+               || button7.Text == "X" && button9.Text == "X" && button8.Text == "X"
+               || button1.Text == "X" && button4.Text == "X" && button7.Text == "X"
+               || button2.Text == "X" && button5.Text == "X" && button8.Text == "X"
+               || button3.Text == "X" && button6.Text == "X" && button9.Text == "X"
+               || button1.Text == "X" && button5.Text == "X" && button9.Text == "X"
+               || button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                yapayZekaHamleleri.Stop(); // Zamanlayıcıyı durdurur
+                MessageBox.Show("Oyuncu Kazandı"); // Oyuncunun kazandığını gösterir
+                oyuncuKazanma++; // Oyuncunun kazandığı oyun sayısını artırır
+                label1.Text = "Oyuncu Kazanma: " + oyuncuKazanma; // Oyuncu kazançlarını günceller
+                OyunuTekrarBaslat(); // Oyunu sıfırlar
+            }
+            else if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O"
+                  || button4.Text == "O" && button5.Text == "O" && button6.Text == "O"
+                  || button7.Text == "O" && button9.Text == "O" && button8.Text == "O"
+                  || button1.Text == "O" && button4.Text == "O" && button7.Text == "O"
+                  || button2.Text == "O" && button5.Text == "O" && button8.Text == "O"
+                  || button3.Text == "O" && button6.Text == "O" && button9.Text == "O"
+                  || button1.Text == "O" && button5.Text == "O" && button9.Text == "O"
+                  || button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                yapayZekaHamleleri.Stop(); // Zamanlayıcıyı durdurur
+                MessageBox.Show("Bilgisayar Kazandı"); // Bilgisayarın kazandığını gösterir
+                bilgisayarKazanma++; // Bilgisayarın kazandığı oyun sayısını artırır
+                label2.Text = "Bilgisayar Kazanma: " + bilgisayarKazanma; // Bilgisayar kazançlarını günceller
+                OyunuTekrarBaslat(); // Oyunu sıfırlar
+            }
+        }
+    }
+}
